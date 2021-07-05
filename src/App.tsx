05efecbaaee9bcc056e31040326c199330399e86
Eugene <aethelz@users.noreply.github.com>
2021-07-05 19:45:26 +0300

    Add redux and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Question from "./Question";
+import typedData from './shared/data';
+import QuestionWrapper from "../QuestionWrapper";
+
 
 function App() {
+  const data = typedData.questionnaire.questions;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((q) => (
+        <QuestionWrapper data={q}>
+          <Question data={q} />
+        </QuestionWrapper >
+      ))}
+      ;
     </div>
   );
 }
